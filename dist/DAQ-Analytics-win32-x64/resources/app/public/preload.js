a4e94d08afb0e3275b0333a375159916b3815277
Rohit Paul <rohit.paul@excelloite.com>
2025-04-05 09:25:03 +0530

    scrollbar fixed

--- dist/DAQ-Analytics-win32-x64/resources/app/public/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electron", {
-  minimize: () => ipcRenderer.send("minimize-window"),
-  maximize: () => ipcRenderer.send("maximize-window"),
-  close: () => ipcRenderer.send("close-window"),
-});
